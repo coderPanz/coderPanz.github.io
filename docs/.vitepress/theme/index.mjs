f2dbe9191198c078a557a92d4a88996f2c1cec46
學吳紫荆 <125804580+coderPanz@users.noreply.github.com>
2024-08-06 22:42:11 +0800

    博客迁移

---
+++ docs/.vitepress/theme/index.mjs
@@ -0,0 +1,23 @@
+import Theme from "vitepress/theme"
+// import "./custom-font.css"
+import "./style/var.css"
+import "viewerjs/dist/viewer.min.css"
+import imageViewer from "vitepress-plugin-image-viewer"
+import vImageViewer from "vitepress-plugin-image-viewer/lib/vImageViewer.vue"
+import { useRoute } from "vitepress"
+
+export default {
+  ...Theme,
+  enhanceApp(ctx) {
+    Theme.enhanceApp(ctx)
+    // Register global components, if you don't want to use it, you don't need to add it
+    ctx.app.component("vImageViewer", vImageViewer)
+    // ...
+  },
+  setup() {
+    // Get route
+    const route = useRoute()
+    // Using
+    imageViewer(route)
+  },
+}
